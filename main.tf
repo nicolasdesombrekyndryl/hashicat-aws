df24e79005a3f5b6e8ad69e228f4259c9fe26a39
Nicolas DESOMBRE <nicolas.desombre@kyndryl.com>
2022-11-22 14:31:17 +0000

    Added the second tag

--- main.tf
+++ main.tf
@@ -128,8 +128,8 @@ resource "aws_instance" "hashicat" {
 
   tags = {
     Name = "${var.prefix}-hashicat-instance"
-    Department = "devops"
-    Billable = "devops"
+    Department = "Devops"
+    Billable = "Devops"
   }
 }
 
